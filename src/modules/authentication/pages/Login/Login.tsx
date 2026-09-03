4333306672bd50bb20dd6ee635c4d1b5e12d2d68
Harsh <harsh123@gmail.com>
2026-09-03 16:41:59 +0530

    feat(auth): modern split-screen authentication with Register, Login, and OTP screens

--- src/modules/authentication/pages/Login/Login.tsx
+++ src/modules/authentication/pages/Login/Login.tsx
@@ -1,26 +1,53 @@
-import { Link } from 'react-router-dom'
-
-import { routePaths } from '@core/config'
-
-import { LoginForm } from '../../components/LoginForm/LoginForm'
-import { useAuth } from '../../hooks/useAuth'
+import { AuthBrandLogo } from '../../components/AuthBrandLogo/AuthBrandLogo'
+import { AuthMetricCards } from '../../components/AuthMetricCards/AuthMetricCards'
+import { AuthPedestal } from '../../components/AuthPedestal/AuthPedestal'
+import { AuthTrustBadges } from '../../components/AuthTrustBadges/AuthTrustBadges'
+import { SignInCard } from '../../components/SignInCard/SignInCard'
 import './Login.css'
 
 export const Login = () => {
-  const { login } = useAuth()
-
   return (
-    <div className="login-page">
-      <header className="login-page__header">
-        <h1 className="login-page__title">Welcome back</h1>
-        <p className="login-page__subtitle">Sign in to manage your filings and applications.</p>
-      </header>
+    <div className="login-screen">
+      {/* Left Stage */}
+      <section className="login-screen__left">
+        <div className="login-screen__brand">
+          <AuthBrandLogo />
+        </div>
 
-      <LoginForm onSubmit={login} />
+        <div className="login-screen__hero-text">
+          <h1 className="login-screen__headline">
+            Welcome back to <span className="login-screen__headline-orange">TaxEdge</span>
+          </h1>
+          <p className="login-screen__subtext">
+            <span>Sign in to track your applications, upload documents and</span>
+            <span>talk to your executive.</span>
+          </p>
+        </div>
 
-      <p className="login-page__footer">
-        New to TaxEdge? <Link to={routePaths.auth.register}>Create an account</Link>
-      </p>
+        {/* 3D Pedestal with Phone, Tax sheet & Shield */}
+        <div className="login-screen__visual-box">
+          <AuthPedestal variant="signIn" />
+        </div>
+
+        {/* 3 Stat Counter Cards */}
+        <AuthMetricCards />
+
+        {/* Bottom Trust Badges */}
+        <AuthTrustBadges />
+      </section>
+
+      {/* Center Dividing Vertical Line with Glowing Orange Ring Node */}
+      <div className="login-screen__center-divider" aria-hidden="true">
+        <div className="login-screen__divider-line" />
+        <div className="login-screen__divider-node" />
+      </div>
+
+      {/* Right Stage: White Card */}
+      <aside className="login-screen__right">
+        <div className="login-screen__card-box">
+          <SignInCard />
+        </div>
+      </aside>
     </div>
   )
 }
